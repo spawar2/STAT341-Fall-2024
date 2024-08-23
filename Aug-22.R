701af0b57b2323b7694e7e5f9c2f275e9a6e4ca6
Pawar <pawar1550@gmail.com>
2024-08-23 08:41:54 -0400

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,12 @@
+#Author:Pawar, Date:08/22/24, Purpose:Test the measures of descriptive stats
+
+# How to calcuate standard deviation in R
+X <- c(1,2,3,4,5)
+sd(X)
+
+# How to calculate mean in R
+mean(X)
+
+# How to calculate median in R
+median(X)
+
