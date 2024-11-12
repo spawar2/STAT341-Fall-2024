3db5220e44c9ef01497ea57d79929e518ca880c2
Pawar <pawar1550@gmail.com>
2024-11-12 16:55:34 -0500

    Add files via upload

---
+++ Probability-distributions-functions.R
@@ -0,0 +1,15 @@
+# Author: Pawar, date: 11/12/2024, Purpose: Calculate probability distributions.
+
+# Function for binomial distribution 
+dbinom(4, size=12, prob=0.2)
+[1] 0.1328756
+
+# Function for Poisson distribution 
+ppois(16, lambda=12)
+[1] 0.898709
+
+# Uniform distribution 
+runif(10, min=1, max=3) 
+ [1] 1.017270 2.247037 1.093897 1.479982
+ [5] 2.915009 1.881633 1.780786 1.095599
+ [9] 1.884352 2.080578
